2610e7f91faf154b027bc1e2fe834bc45fc45682
Eyasdm <eyasadam878@gmail.com>
2025-10-07 04:51:07 +0700

    Improve popup animation and validation

--- src/components/WorkoutForm.jsx
+++ src/components/WorkoutForm.jsx
@@ -4,41 +4,66 @@
 
 import { FaTrash } from "react-icons/fa";
 import { AddWorkoutBtn, Popup } from "./Popup";
+import { useState } from "react";
 
 export default function WorkoutForm({
   onToggleDone,
   onToggleDelete,
   onAddNewWorkout,
   filtered,
+  currentDay,
 }) {
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
+
   const sortedWorkouts = [...filtered].sort((a, b) => {
-    if (a.done === b.done) {
-      return a.order - b.order;
-    }
+    if (a.done === b.done) return a.order - b.order;
     return a.done - b.done;
   });
 
   return (
     <>
-      <div className="form">
-        {sortedWorkouts.map((w) => (
-          <WorkoutItem
-            key={w.order}
-            name={w.name}
-            part={w.part}
-            sets={`${w.sets} sets x ${w.reps} reps @ ${w.weight}kg`}
-            id={w.id}
-            onToggleDone={onToggleDone}
-            onToggleDelete={onToggleDelete}
-            done={w.done}
-          />
-        ))}
-      </div>
-      <AddWorkoutBtn />
-      <Popup onAddWorkout={onAddNewWorkout} />
+      {(!filtered || filtered.length === 0) && (
+        <div className="workout-form__empty">
+          <p className="workout-form__empty-text">
+            No workouts added for this day 💪
+            <span>Add one using the “+” button below.</span>
+          </p>
+        </div>
+      )}
+
+      {filtered && filtered.length > 0 && (
+        <div className="form">
+          {sortedWorkouts.map((w) => (
+            <WorkoutItem
+              key={w.id}
+              name={w.name}
+              part={w.part}
+              sets={`${w.sets} sets x ${w.reps} reps @ ${w.weight}kg`}
+              id={w.id}
+              onToggleDone={onToggleDone}
+              onToggleDelete={onToggleDelete}
+              done={w.done}
+            />
+          ))}
+        </div>
+      )}
+
+      <AddWorkoutBtn onIsPopupOpen={setIsPopupOpen} />
+
+      {isPopupOpen && (
+        <Popup
+          currentDay={currentDay}
+          onAddWorkout={(workout) => {
+            onAddNewWorkout(workout);
+            setIsPopupOpen(false);
+          }}
+          onClose={() => setIsPopupOpen(false)}
+        />
+      )}
     </>
   );
 }
+
 function WorkoutItem({
   name,
   part,
